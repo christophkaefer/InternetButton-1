227e23d9e140ea9fdaaac4cd98257de6723cfdbe
Christl Schallhammer <bruzel@gmx.at>
2016-02-06 10:51:49 +0100

    InternetButtonTest Klasse

---
+++ swe-iot/src/main/java/org/c02/iot/InternetButtonTest.java
@@ -0,0 +1,33 @@
+package org.c02.iot; 
+ 
+ 
+import org.c02.iot.cloud.api.ParticleApiWrapper; 
+import org.c02.iot.cloud.api.ParticleException; 
+import org.junit.Before; 
+import org.junit.Test; 
+import org.junit.runner.RunWith; 
+import org.mockito.Mock; 
+import org.mockito.Mockito; 
+import org.mockito.runners.MockitoJUnitRunner; 
+
+ 
+@RunWith(MockitoJUnitRunner.class) 
+public class InternetButtonTest { 
+ 
+private InternetButtonApi internetButtonApi; 
+ 
+ 
+@Mock 
+private ParticleApiWrapper particleWrapperApi; 
+ 
+@Before 
+public void setup() { 
+	internetButtonApi = new InternetButtonImpl(particleWrapperApi); 
+ 	} 
+
+ 	@Test 
+ 	public void testPlaySound() throws ParticleException { 
+		internetButtonApi.playSound(); 
+		Mockito.verify(particleWrapperApi).callMethod("play", null); 
+ 	} 
+ } 
