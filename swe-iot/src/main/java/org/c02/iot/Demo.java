7d71d9ee811a3a3875ee15465b08e7f08c48c617
Christl Schallhammer <bruzel@gmx.at>
2016-02-06 11:06:51 +0100

    Demo Klasse mit dem Test für Melodie

---
+++ swe-iot/src/main/java/org/c02/iot/Demo.java
@@ -0,0 +1,19 @@
+package org.c02.iot;
+
+import org.c02.iot.cloud.api.ParticleApiWrapper;
+import org.c02.iot.cloud.api.ParticleApiWrapperImpl;
+import org.c02.iot.cloud.examples.DemoConstants;
+
+public class Demo {
+	
+	static ParticleApiWrapper api = new ParticleApiWrapperImpl(DemoConstants.deviceId,
+			DemoConstants.accessToken);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	InternetButtonImpl button=new InternetButtonImpl(api);
+	button.playSound();	
+
+	}
+
+}
