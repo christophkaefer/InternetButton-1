ca3f689b6758f66fd9ecf99a07a68a288a145d56
Christl Schallhammer <bruzel@gmx.at>
2016-02-06 12:34:09 +0100

    Button Counter

--- swe-iot/src/main/java/org/c02/iot/InternetButtonImpl.java
+++ swe-iot/src/main/java/org/c02/iot/InternetButtonImpl.java
@@ -1,6 +1,7 @@
 package org.c02.iot;
 
 import java.awt.Color;
+import java.io.IOException;
 
 import org.c02.iot.cloud.api.ParticleApiWrapper;
 import org.c02.iot.cloud.api.ParticleException;
@@ -15,7 +16,36 @@ public class InternetButtonImpl implements InternetButtonApi {
 
 	public int getButtonCounter(ButtonDirection button) 
 	{
-		// TODO Auto-generated method stub
+		
+
+			if (button==ButtonDirection.North)
+				try {
+					return wrapper.readVariable("countButton1");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (button==ButtonDirection.South)
+				try {
+					return wrapper.readVariable("countButton2");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (button==ButtonDirection.East)
+				try {
+					return wrapper.readVariable("countButton3");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+			if (button==ButtonDirection.West)
+				try {
+					return wrapper.readVariable("countButton4");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 		return 0;
 	}
 
