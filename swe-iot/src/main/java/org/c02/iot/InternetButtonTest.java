b6467e5a12fb9fb0f7c631bf202a4e450a919e25
christoph.kaefer <christoph.kaefer@edu.campus02.at>
2016-02-06 12:45:54 +0100

    TestMethode implementiert

--- swe-iot/src/main/java/org/c02/iot/InternetButtonTest.java
+++ swe-iot/src/main/java/org/c02/iot/InternetButtonTest.java
@@ -1,38 +1,84 @@
-package org.c02.iot; 
- 
- 
-import org.c02.iot.cloud.api.ParticleApiWrapper; 
-import org.c02.iot.cloud.api.ParticleException; 
-import org.junit.Before; 
-import org.junit.Test; 
-import org.junit.runner.RunWith; 
-import org.mockito.Mock; 
-import org.mockito.Mockito; 
-import org.mockito.runners.MockitoJUnitRunner; 
+package org.c02.iot;
 
- 
-@RunWith(MockitoJUnitRunner.class) 
-public class InternetButtonTest { 
- 
-private InternetButtonApi internetButtonApi; 
- 
- 
-@Mock 
-private ParticleApiWrapper particleWrapperApi; 
- 
-@Before 
-public void setup() { 
-	internetButtonApi = new InternetButtonImpl(particleWrapperApi); 
- 	} 
+import java.awt.Color;
+import java.io.IOException;
 
- 	@Test 
- 	public void testPlaySound() throws ParticleException { 
-		internetButtonApi.playSound(); 
-		Mockito.verify(particleWrapperApi).callMethod("play", null); 
- 	} 
- 	@Test 
- 	public void testSetLed() throws ParticleException { 
-		internetButtonApi.setLed(0, null); 
-		Mockito.verify(particleWrapperApi).callMethod("led", "03255255000"); 
- 	} 
- } 
+import org.c02.iot.cloud.api.ParticleApiWrapper;
+import org.c02.iot.cloud.api.ParticleException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InternetButtonTest {
+
+	private InternetButtonApi internetButtonApi;
+
+	@Mock
+	private ParticleApiWrapper particleWrapperApi;
+
+	@Before
+	public void setup() {
+		internetButtonApi = new InternetButtonImpl(particleWrapperApi);
+	}
+
+	@Test
+	public void testPlaySound() throws ParticleException {
+		internetButtonApi.playSound();
+		Mockito.verify(particleWrapperApi).callMethod("play", null);
+	}
+
+	@Test
+	public void testSetLedIntface() throws ParticleException {
+		ParticleApiWrapper mocApi = new ParticleApiWrapper() {
+
+			@Override
+			public int readVariable(String variable) throws IOException {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int callMethod(String method, String parameter) throws ParticleException {
+
+				Assert.assertEquals("led", method);
+				Assert.assertEquals("10255255255", parameter);
+
+				return 0;
+			}
+		};
+
+		internetButtonApi = new InternetButtonImpl(mocApi);
+		internetButtonApi.setLed(10, Color.WHITE);
+
+	}
+
+	@Test
+	public void testSetLedRedIntface() throws ParticleException {
+//setup
+		ParticleApiWrapper mocApi = new ParticleApiWrapper() {
+			@Override
+			public int readVariable(String variable) throws IOException {
+				return 0;
+			}
+
+			@Override
+			public int callMethod(String method, String parameter) throws ParticleException {
+//verify
+				Assert.assertEquals("led", method);
+				Assert.assertEquals("11255000000", parameter);
+
+				return 0;
+			}
+		};
+
+		internetButtonApi = new InternetButtonImpl(mocApi);
+		//execute
+		internetButtonApi.setLed(11, Color.RED);
+
+	}
+}
