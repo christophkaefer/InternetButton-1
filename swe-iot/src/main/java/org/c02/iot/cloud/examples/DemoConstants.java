a13f148f2aaaa021f58be739acb0520befaa2a3f
christoph.kaefer <christoph.kaefer@edu.campus02.at>
2016-02-06 12:21:43 +0100

    Iteration 3

--- swe-iot/src/main/java/org/c02/iot/cloud/examples/DemoConstants.java
+++ swe-iot/src/main/java/org/c02/iot/cloud/examples/DemoConstants.java
@@ -1,6 +1,7 @@
 package org.c02.iot.cloud.examples;
 
-public interface DemoConstants {
+public interface DemoConstants 
+{
 	String deviceId = "350042001347343432313031";
 	String accessToken = "2a9b72a4491967cb19d2ba10b07c40cc662dadf8";
 
